41fd943bff4e6711b7e2052247bb2d9a92633f64
Viktor.Kyarginsky <Viktor.Kyarginsky@x5.ru>
2021-03-25 15:01:12 +0300

    Homework #8: подготовительный этап

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,7 +1,7 @@
 import s from './pockemoncard.module.css'
 import cn from 'classnames'
 
-const PokemonCard = ({name, img, keyId, id, type, values, isActive, minimize, className, isSelected, onClick}) => {
+const PokemonCard = ({name, img, keyId, id, type, values, isActive, minimize, className, isSelected, possession, onClick}) => {
 
     const handleClick = () => {
         isActive = !isActive
@@ -14,7 +14,7 @@ const PokemonCard = ({name, img, keyId, id, type, values, isActive, minimize, cl
              onClick={handleClick}>
             <div className={s.cardFront}>
                 <div className={cn(s.wrap, s.front)}>
-                    <div className={cn(s.pokemon, s[type])}>
+                    <div className={cn(s.pokemon, s[type], s[possession])}>
                         <div className={s.values}>
                             <div className={cn(s.count, s.top)}>{values.top}</div>
                             <div className={cn(s.count, s.right)}>{values.right}</div>
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,7 +2,7 @@ import '../../App.css';
 import Header from '../../components/Header';
 import Layout from '../../components/Layout';
 
-// import bgImage1 from './images/bg1.jpg'
+// import bgImage1 from '../../images/bg1.jpg'
 import bgImage2 from '../../images/bg2.jpg'
 import bgImage3 from '../../images/bg3.jpg'
 
@@ -11,11 +11,11 @@ const HomePage = () => {
 
     return (
         <>
-            <Header title="Super Puper Poke"
-                    descr="This is not only the game"
+            <Header title="Triple Triad"
+                    descr="коллекционная карточная игра"
             >
             </Header>
-            <Layout title="Rule" urlBg={bgImage2}>
+            <Layout title="Rules" urlBg={bgImage3}>
                 <p>In the game two players face off against one another, one side playing as "blue", the other as "red"
                     on a 3x3 grid.
                     Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them
@@ -27,8 +27,21 @@ const HomePage = () => {
                     card will be captured and turned into the opponent's color. If the player's rank is higher, the
                     opponent's card will be captured and changed into the player's color instead. </p>
             </Layout>
-            <Layout title="Description of the game" urlBg={bgImage3}>
-                <p>Coming soon...</p>
+            <Layout title="Описание правил игры" urlBg={bgImage2}>
+                <p>Triple Triad - коллекционная карточная игра, т.е. состав колоды не задается жестко, а постоянно
+                    пополняется. В партии принимают участие два человека, каждый со своей колодой. Игровое поле
+                    представляет собой доску размером 3 * 3. Игроки просматривают свои колоды, набирают из них в руку по
+                    пять любых карт и бросают жребий, кому ходить первому. Ходы делаются строго по очереди: участник
+                    выбирает карту в руке и кладет ее на одну из свободных клеток, при этом она (карта) окрашивается в
+                    его цвет.</p>
+
+                <p>Партия начинается со счета 5 : 5. Когда игрок А выкладывает свою карту рядом с уже помещенной на поле
+                    картой игрока B (в соседнюю клетку по горизонтали или вертикали), производится сравнение чисел на
+                    соприкасающихся сторонах: если число карты A больше, игрок переворачивает карту B и перекрашивает в
+                    свой цвет. При этом счет изменяется следующим образом - A получает бонус в +1 очко, а B, наоборот,
+                    на -1 очко штрафуется. В нашем примере счет станет 6 : 4 в пользу A. Можно переворачивать и по
+                    нескольку карт одновременно. Когда все девять клеток поля будут заняты, игра заканчивается.
+                    Победителем становится игрок с большим числом очков.</p>
             </Layout>
         </>
     );
